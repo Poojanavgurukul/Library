57e743ba5ff3fba154bea41af56c30d719a19f69
Poojanavgurukul <Pooja17@navgurukul.org>
2019-09-17 15:41:04 +0530

    add. borrow method

--- src/main/java/library/Book.java
+++ src/main/java/library/Book.java
@@ -4,9 +4,12 @@ package library;
 public class Book {
     public String name;
     public String category;
-
-    public Book(String name, String category) {
+    public String isbn;
+    public boolean isAvailable;
+    public Book(String name, String category,String isbn,boolean isAvailable) {
         this.name = name;
         this.category = category;
+        this.isbn=isbn;
+        this.isAvailable=isAvailable;
     }
 }
--- src/main/java/library/Borrower.java
+++ src/main/java/library/Borrower.java
@@ -1,5 +1,16 @@
 package library;
 
-public class Borrower {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Borrower {
+    List<Book>bag=new ArrayList<>();
+    public void borrow(Book book){
+        if (book.isAvailable){
+            bag.add(book);
+        }
+    }
+    public List<Book> getBag() {
+        return bag;
+    }
 }
