8f18af46af7c5df696bb40e09516a84a957afffd
Poojanavgurukul <Pooja17@navgurukul.org>
2019-09-17 12:31:55 +0530

    add borrower class and BookDetails and constructor

---
+++ src/main/java/library/Borrower.java
@@ -0,0 +1,10 @@
+package library;
+
+import java.util.List;
+
+public class Borrower {
+    List<BookDetail>details;
+    public void borrow(Book book){
+
+    }
+}
--- src/test/java/library/Library.java
+++ src/test/java/library/Library.java
@@ -1,7 +1,6 @@
 package library;
 
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 
